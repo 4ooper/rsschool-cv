0054587bd2c39835959cf586ee23f3e526e74042
4ooper <79720254+4ooper@users.noreply.github.com>
2021-04-24 16:59:58 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const inputs = document.querySelectorAll(".filters input");
-
-function handleUpdate()
-    {
-        console.log(this.value);
-    }
-
-inputs.forEach(input => input.addEventListener('change',handleUpdate));
